8420140311f02731169a5e43d786fd218e76929a
Jason <1109958447@qq.com>
2020-01-19 17:52:53 +0800

    up

--- src/IP.php
+++ src/IP.php
@@ -1,15 +1,6 @@
 <?php
 
 namespace SmartJson\IpLocationZh;
-/*
-    全球 IPv4 地址归属地数据库(IPIP.NET 版)
-    高春辉(pAUL gAO) <gaochunhui@gmail.com>
-    Build 20170905 版权所有 IPIP.NET
-    (C) 2006 - 2017 保留所有权利，北京天特信科技有限公司
-    本代码仅用于 DAT 格式，请注意及时更新 IP 数据库版本
-    数据问题请加 QQ 交流群: 346280296
-    Code for PHP 5.3+ only!
-*/
 
 class IP
 {
@@ -19,65 +10,40 @@ class IP
     private static $offset = NULL;
     private static $index = NULL;
 
-    public static function find($ip)
+    public static function find($ip, $language = 'CN')
     {
         if (empty($ip) === TRUE) {
             return 'N/A';
         }
 
-        $nip = gethostbyname($ip);
-        $ipdot = explode('.', $nip);
+        $baseStation = self::init();
+        return $baseStation->find($ip, $language);
+    }
 
-        if ($ipdot[0] < 0 || $ipdot[0] > 255 || count($ipdot) !== 4) {
+    public static function findInfo($ip, $language = 'CN')
+    {
+        if (empty($ip) === TRUE) {
             return 'N/A';
         }
 
-        if (self::$fp === NULL) {
-            self::init();
-        }
+        $baseStation = self::init();
+        return $baseStation->findInfo($ip, $language);
+    }
 
-        $nip2 = pack('N', ip2long($nip));
-
-        $tmp_offset = (int)$ipdot[0] * 4;
-        $start = unpack('Vlen', self::$index[$tmp_offset] . self::$index[$tmp_offset + 1] . self::$index[$tmp_offset + 2] . self::$index[$tmp_offset + 3]);
-
-        $index_offset = $index_length = NULL;
-        $max_comp_len = self::$offset['len'] - 1024 - 4;
-        for ($start = $start['len'] * 8 + 1024; $start < $max_comp_len; $start += 8) {
-            if (self::$index{$start} . self::$index{$start + 1} . self::$index{$start + 2} . self::$index{$start + 3} >= $nip2) {
-                $index_offset = unpack('Vlen', self::$index{$start + 4} . self::$index{$start + 5} . self::$index{$start + 6} . "\x0");
-                $index_length = unpack('Clen', self::$index{$start + 7});
-
-                break;
-            }
-        }
-
-        if ($index_offset === NULL) {
+    public static function findMap($ip, $language = 'CN')
+    {
+        if (empty($ip) === TRUE) {
             return 'N/A';
         }
 
-        fseek(self::$fp, self::$offset['len'] + $index_offset['len'] - 1024);
-
-        return explode("\t", fread(self::$fp, $index_length['len']));
+        $baseStation = self::init();
+        return $baseStation->findMap($ip, $language);
     }
 
     private static function init()
     {
-        if (self::$fp === NULL) {
-            self::$ip = new self();
-
-            self::$fp = fopen(__DIR__ . '/17monipdb.dat', 'rb');
-            if (self::$fp === FALSE) {
-                throw new Exception('Invalid 17monipdb.dat file!');
-            }
-
-            self::$offset = unpack('Nlen', fread(self::$fp, 4));
-            if (self::$offset['len'] < 4) {
-                throw new Exception('Invalid 17monipdb.dat file!');
-            }
-
-            self::$index = fread(self::$fp, self::$offset['len'] - 4);
-        }
+        $baseStation = new BaseStation(__DIR__ . '/ipipfree.ipdb');
+        return $baseStation;
     }
 
     public function __destruct()
